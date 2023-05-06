fb50ec33fcf571e3cdb91fbc5138b74d93713123
dd <12862256+dd123j@user.noreply.gitee.com>
2023-05-06 09:37:23 +0800

    dj hotfix first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("second ");
-        System.out.println("third ");
+        System.out.println("Hello Git! dj hotfix first modify");
         System.out.println("Hello Git! dj master first modify");
 
     }
