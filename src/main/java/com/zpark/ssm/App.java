113f147ecf9587eb1e72884ea5b77e64e24ac42d
dd <12862256+dd123j@user.noreply.gitee.com>
2023-05-06 09:40:54 +0800

    dj hotfix first commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,7 +3,7 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("second ");
+        System.out.println("Hello Git! dj hotfix first commit");
         System.out.println("Hello Git! dj hotfix first modify");
         System.out.println("Hello Git! dj master first modify");
 
