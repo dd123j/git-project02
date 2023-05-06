ec16bc0b91f67fe2ef952417d58c304e3227bdce
dd <12862256+dd123j@user.noreply.gitee.com>
2023-05-06 09:33:21 +0800

    dj master first modify

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,6 +5,7 @@ public class App {
         System.out.println("Hello Git!");
         System.out.println("second ");
         System.out.println("third ");
+        System.out.println("Hello Git! dj master first modify");
 
     }
 }
